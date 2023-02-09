b6867c46d269438ce7dc24ee4ce98d12a732e288
gaoyanfeng <836000498@qq.com>
2023-02-09 08:35:11 +0800

    1

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import VueSetupExtend from 'vite-plugin-vue-setup-extend';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+const { resolve } = require('path')
+export default defineConfig({
+	base: './',
+	plugins: [
+		vue(),
+		VueSetupExtend(),
+		AutoImport({
+			resolvers: [ElementPlusResolver()]
+		}),
+		Components({
+			resolvers: [ElementPlusResolver()]
+		})
+	],
+	resolve: {
+		alias: [
+			{
+				find: "@",
+				replacement: resolve(__dirname, 'src')
+			}
+		]
+	},
+	optimizeDeps: {
+		include: ['schart.js']
+	}
+});
